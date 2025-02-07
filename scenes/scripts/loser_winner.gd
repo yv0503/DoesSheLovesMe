15470b5c30d28c53d78ce91f7b21da672f6ca6d4
yv0503 <yvanstephen0503@gmail.com>
2025-02-08 02:33:05 +0800

    Added Basic Lighting

--- scenes/scripts/loser_winner.gd
+++ scenes/scripts/loser_winner.gd
@@ -1,20 +1,20 @@
 extends Node
 
-var rndint = RandomNumberGenerator.new()
+var random = RandomNumberGenerator.new()
 var num = 0
 var win_lose: bool
-var qr_text: String = "this thing fucking works"
+var qr_text: String = "09erwui34290ersdlk"
 var error_correction_level: int = 0
 
 func _ready() -> void:
 	randomWinner()
 
 func randomWinner():
-	rndint.randomize()
-	var random = rndint.randi_range(0,100)
+	random.randomize()
+	var rndint = random.randi_range(0,100)
 	for x in range(0,50):
-		if x == random:
+		if x == rndint:
 			win_lose = false
 	for x in range(51,100):
-		if x == random:
+		if x == rndint:
 			win_lose = true
